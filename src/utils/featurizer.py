a9c317a6573621e9d2a5fb7ca6a4dedfe8934163
hspark1212 <phs68660888@gmail.com>
2025-10-01 17:31:27 +0100

    update default vae path for featurizer

--- src/utils/featurizer.py
+++ src/utils/featurizer.py
@@ -7,7 +7,7 @@ from src.data.dataset_util import pmg_structure_to_pyg_data
 from src.vae_module.vae_module import VAEModule
 from src.utils.scatter import scatter_mean, scatter_sum
 
-DEFAULT_MODEL_PATH = "ckpts/mp_20/vae/dng_m4owq4i5_v0.ckpt"  # TODO: update this path
+DEFAULT_MODEL_PATH = "benchmarks/assets/dng_m4owq4i5_v0.ckpt"  # TODO: update this path
 
 
 def featurize(
